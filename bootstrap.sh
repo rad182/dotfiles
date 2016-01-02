9030b35e9e08fcfcecdf279e931c60823bf83128
rad182 <rady182@me.com>
2016-01-02 23:09:16 +0800

    added cask cleanup

--- bootstrap.sh
+++ bootstrap.sh
@@ -44,6 +44,7 @@ function rvm() {
 		gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 		\curl -sSL https://get.rvm.io | bash -s stable --ruby
 	fi
+	brew cask cleanup
 }
 
 function rubygems() {
