5c43f1bd46b878728dc16d44c1cad0329daabf36
rad182 <rady182@me.com>
2016-01-02 19:25:39 +0800

    added more bootstrap methods 1. install homebrew 2. install rvm and gems

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# function
+installed() { hash $1 &> /dev/null; }
+
 cd "$(dirname "${BASH_SOURCE}")";
 
 git pull origin master;
@@ -8,6 +11,44 @@ function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
 		--exclude "README.md" --exclude "LICENSE-MIT.txt" -avh --no-perms . ~;
 	source ~/.bash_profile;
+	# Install Homebrew
+	brew;
+	# Install rvm
+	rvm;
+	# Install gems
+	rubygems;
+}
+
+function brew() {
+	# Homebrew
+	if ! installed brew; then
+		ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+	fi
+
+	# install casks
+	brew install caskroom/cask/brew-cask
+
+	# install brewdler
+	brew tap Homebrew/brewdler
+
+	# install everything from Brewfile
+	brew update
+	brew upgrade --all
+	brew brewdle
+	brew linkapps
+	brew cleanup
+}
+
+function rvm() {
+	if ! installed rvm; then
+		gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+		\curl -sSL https://get.rvm.io | bash -s stable --ruby
+	fi
+}
+
+function rubygems() {
+	gem install bundler
+	bundle install
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
@@ -19,4 +60,8 @@ else
 		doIt;
 	fi;
 fi;
+
 unset doIt;
+unset brew;
+unset rvm;
+unset rubygems;
